c0ef5bbd829fe9321f3537345a149fc80d3bdedb
zhaowen1406410126 <1553219915@qq.com>
2017-11-15 21:20:32 +0800

    3

--- main2048.js
+++ main2048.js
@@ -36,4 +36,84 @@ function newgame() {
 	// 初始化棋盘格
 	init();
 	// 随机生成两个数字
-	generateOneNumb
+	generateOneNumber();
+	generateOneNumber();
+}
+// 初始化
+function init() {
+
+	// 棋盘小格子位置 初始化
+	for (var i = 0; i < 4; i++) {
+		for (var j = 0; j < 4; j++) {
+
+			var gridCell = $("#grid-cell-" + i + "-" + j);
+			gridCell.css("top", getPosTop(i, j));
+			gridCell.css("left", getPosLeft(i, j));
+		}
+	}
+	// 初始值为零
+	for (var i = 0; i < 4; i++) {
+		board[i] = new Array();
+		hasConflicted[i] = new Array();
+		for (var j = 0; j < 4; j++) {
+			board[i][j] = 0;
+			hasConflicted[i][j] = false;
+		}
+	}
+	// 初始化分数
+	score = 0;
+	updateScore(score);
+	updateBoardView();
+}
+// 刷新数据
+function updateBoardView() {
+
+	$(".number-cell").remove();
+
+	for (var i = 0; i < 4; i++) {
+		for (var j = 0; j < 4; j++) {
+			$("#grid-container").append("<div class=\"number-cell\" id=\"number-cell-" + i + "-" + j + "\"></div>")
+
+			var theNumberCell = $("#number-cell-" + i + "-" + j);
+			// number-cell 样式
+
+			if (board[i][j] == 0) { // 这个格子没有数字时
+				theNumberCell.css("top", getPosTop(i, j) + cellSideLength / 2);
+				theNumberCell.css("left", getPosLeft(i, j) + cellSideLength / 2);
+				theNumberCell.css("width", "0px");
+				theNumberCell.css("height", "0px");
+			} else { // 这个格子有数字时
+				theNumberCell.css("top", getPosTop(i, j));
+				theNumberCell.css("left", getPosLeft(i, j));
+				theNumberCell.css("width", cellSideLength);
+				theNumberCell.css("height", cellSideLength);
+				theNumberCell.css("color", getNumberColor(board[i][j]));
+				theNumberCell.css("background-color", getBackgroundColor(board[i][j]));
+				theNumberCell.text(board[i][j]);
+			}
+			hasConflicted[i][j] = false;
+		}
+	}
+	$(".number-cell").css("line-height", cellSideLength + "px")
+	$(".number-cell").css("font-size", 0.4 * cellSideLength + "px")
+
+}
+// 随机生成数字
+function generateOneNumber() {
+	// 如果没有空余格子了
+	if (nospace(board)) {
+		return false;
+	}
+	// 如果有
+	// 生成随机格子
+	var randx = Math.floor(Math.random() * 4);
+	var randy = Math.floor(Math.random() * 4);
+
+	var times = 0;
+	while (times < 50) {
+		if (board[randx][randy] == 0) {
+			break;
+		} else {
+			randx = Math.floor(Math.random() * 4);
+			randy = Math.floor(Math.random() * 4);
+		}
